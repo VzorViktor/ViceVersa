55ae7db4d48b1db5518b2aa12a6e377b1e294cda
Viktor Bondarovich <ponkjkeee@gmail.com>
2022-10-05 17:18:39 +0300

    Work "home"

--- viceversa/viceversa/urls.py
+++ viceversa/viceversa/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('home/', views.home),
-
 ]
--- viceversa/viceversa/views.py
+++ viceversa/viceversa/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def about(request):
     return HttpResponse('This is about page')
 
 def home(request):
-    return render(request, 'home.html')
+    return HttpResponse('My home')
+
+# def home(request):
+#     return render(request, 'home.html', {'greeting': 'Hello'})
